d6695820fdc623d10d84e2a1a7e5521d878716c7
Eric Gusmao <ericdegusmaopino@gmail.com>
2025-03-09 23:40:36 -0300

    Add fisherYatesShuffle macro to Collection for shuffling items

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Collection::macro('fisherYatesShuffle', function () {
+            $items = $this->items;
+
+            for ($i = count($items) - 1; $i > 0; $i--) {
+                $j = random_int(0, $i);
+                [$items[$i], $items[$j]] = [$items[$j], $items[$i]];
+            }
+
+            return new static($items);
+        });
     }
 }
