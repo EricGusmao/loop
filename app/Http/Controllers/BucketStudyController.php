dcb4d81afc31728602070f2212868b3ceb898582
Eric Gusmao <ericdegusmaopino@gmail.com>
2025-03-10 11:52:00 -0300

    Add return type declaration for index method in BucketStudyController

--- app/Http/Controllers/BucketStudyController.php
+++ app/Http/Controllers/BucketStudyController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\Bucket;
 use Illuminate\Support\Facades\Gate;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class BucketStudyController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(Bucket $bucket)
+    public function index(Bucket $bucket): Response
     {
         Gate::authorize('view', $bucket);
 
